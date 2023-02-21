03c9b06b149549cdefec2b7349946b3dc22e88d7
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2023-02-20 22:43:59 -0500

    installed roboto fonts with expo

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
-import { Groups }  from '@screens/Groups';
-import { ThemeProvider } from 'styled-components/native';
 import themes from './src/themes';
+import { ThemeProvider } from 'styled-components/native';
+import { 
+  useFonts, 
+  Roboto_400Regular, 
+  Roboto_700Bold } 
+  from '@expo-google-fonts/roboto';
+import { ActivityIndicator} from 'react-native';
+import { Groups }  from '@screens/Groups';
 
 export default function App() {
-  return (
-  <ThemeProvider theme={themes}>
-    <Groups/>
 
+  const [fontsLoaded ] = useFonts({
+    Roboto_400Regular, 
+    Roboto_700Bold
+  })
+ 
+  return ( 
+  <ThemeProvider theme={themes}>
+  { !fontsLoaded ? <ActivityIndicator/> :
+  
+    <Groups/>
+  }
   </ThemeProvider>
     )
 }
