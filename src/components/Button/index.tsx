a9879644336d0d0d14ff51ae143d391344fc96dd
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2023-02-23 10:02:21 -0500

    feat: created our System Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,22 @@
+import { Container , ButtonText, ButtonTypeStyleProps} from './styles';
+import { TouchableOpacityProps } from 'react-native';
+
+
+type ButtonProps = TouchableOpacityProps & {
+    buttonText: string;
+    type: ButtonTypeStyleProps;
+}
+
+export const Button = ({type = "PRIMARY", buttonText, ...rest}: ButtonProps)=>{
+    return (
+        <Container
+            type={type} 
+           
+            {...rest}    
+            >
+            <ButtonText>
+                {buttonText}
+            </ButtonText>
+        </Container>
+    )
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,12 +3,13 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { EmptyList } from '@components/EmptyList'
+import { Button } from '@components/Button';
 
 import  { useState } from 'react';
 import { FlatList } from 'react-native';
 
 export function Groups() {
-  const [ groups, setGroups ] = useState<string[]>([]);
+  const [ groups, setGroups ] = useState<string[]>(['L8ters of the Night']);
 
   return (
     <Container>
@@ -24,7 +25,11 @@ export function Groups() {
             description="Please register a group to get started."/>)}
             contentContainerStyle={groups.length === 0 && { flex: 1}}
       />
-   
+
+          <Button
+            buttonText="Create a new group"
+            onPress={()=>{}}
+          />
     
      
     </Container>
