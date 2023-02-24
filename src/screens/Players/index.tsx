820cd7d2ce1990d4394980d795c3433a5756d8e4
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2023-02-24 08:54:51 -0500

    Created the horizontal list showing the Team and number of players per Team

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,12 +1,18 @@
-import { Container, Form } from "./styles";
-import { Header } from "@components/Header";
-import { Highlight } from "@components/Highlight";
-import { ButtonIcon } from "@components/ButtonIcon";
-import { Input } from "@components/Input";
-import { useTheme } from "styled-components/native";
+import { Container, Form, HeaderList, NumberOfPlayersPerTeam } from './styles';
+import { useTheme } from 'styled-components/native';
+import { FlatList } from 'react-native';
+import { Header } from '@components/Header';
+import { useState } from 'react';
+import { Highlight } from '@components/Highlight';
+import { ButtonIcon } from '@components/ButtonIcon';
+import { Input } from '@components/Input';
+import { Filter } from '@components/Filter'
 
 export const Players = () => {
   const { COLORS } = useTheme();
+  const [ team, setTeam ] = useState('Team A');
+  const [ players, setPlayers] =  useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -21,8 +27,31 @@ export const Players = () => {
           autoCorrect={false}
         />
 
-        <ButtonIcon icon="add"/>
+        <ButtonIcon 
+          icon="add"/>
       </Form>
+
+      <HeaderList> 
+        <FlatList
+          horizontal
+          data={['Time A', 'Time B']}
+          keyExtractor={item=> item}
+          renderItem={({item})=> (
+            <Filter 
+              title={item}
+              onPress={()=>setTeam(item)}
+              isActive={item === team}
+            />
+          )
+          
+        }
+        />
+
+        <NumberOfPlayersPerTeam>
+          {players.length}
+        </NumberOfPlayersPerTeam>
+      </HeaderList>
+
     </Container>
   );
 };
