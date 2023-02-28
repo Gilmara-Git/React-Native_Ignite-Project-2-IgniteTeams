e8c6eabb543a17ab430fefd21bff55cdd0c77426
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2023-02-28 10:14:32 -0500

    feat : setup routes and a global routes type

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,35 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Groups } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroup';
+import { Players } from '@screens/Players';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export type AppRoutes = {
+   Groups: undefined;
+   NewGroup: undefined;
+   Players: undefined;
+}
+
+
+export const AppRoutes =()=>{    
+      return ( 
+      <Navigator screenOptions={ {
+         headerShown: false
+         }
+         }
+      >
+         <Screen 
+            name="groups" 
+            component={Groups}/>
+         <Screen 
+            name="new" 
+            component={NewGroup}/>
+         <Screen 
+            name="players" 
+            component={Players}/>
+      </Navigator>
+
+      )
+
+}
